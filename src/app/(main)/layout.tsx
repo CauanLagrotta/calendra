33cdc0d5fc4be78b52fbe464fc3c5011b81fec2e
CauanLagrotta <cauanlagrotta.dev@gmail.com>
2025-09-05 14:21:11 -0300

    feat: creating navbar

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,19 @@
+import PrivateNavbar from "@/components/PrivateNavBar";
+import PublicNavBar from "@/components/PublicNavBar";
+import { currentUser } from "@clerk/nextjs/server";
+
+export default async function MainLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const user = await currentUser();
+
+  return (
+    <main className="relative">
+      {user ? <PrivateNavbar /> : <PublicNavBar />}
+
+      <section className="pt-36">{children}</section>
+    </main>
+  );
+}
