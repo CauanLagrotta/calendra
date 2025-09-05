8a2edac3647be930b1d3e7b812b9e442f13660f5
CauanLagrotta <cauanlagrotta.dev@gmail.com>
2025-09-04 21:29:44 -0300

    build: setting up drizzle

---
+++ drizzle.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "drizzle-kit";
+
+const databaseUrl = process.env.DATABASE_URL;
+
+if (!databaseUrl) {
+  throw new Error("❌ DATABASE_URL is not defined in environment variables.");
+}
+
+export default defineConfig({
+  schema: "./src/drizzle/schema.ts",
+
+  out: "./src/drizzle/migrations",
+
+  dialect: "postgresql",
+
+  strict: true,
+
+  verbose: true,
+
+  dbCredentials: {
+    url: databaseUrl,
+  },
+});
